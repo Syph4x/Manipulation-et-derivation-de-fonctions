20e62cc3db205ec559c3112a8be2aae27133d13b
Syph4x <101002526+Syph4x@users.noreply.github.com>
2023-04-19 17:43:23 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Fonction.h"
+#include "Polynomes.h"
+#include "Constante.h"
+#include "CFonction.h"
+#include "Somme.h"
+#include "Produit.h"
+#include "Puissance.h"
+#include "Interface.h"
+using namespace std;
+
+int main()
+{
+
+ Interface I;
+    I.Interface_Aide();
+ I.Interface_user();
+
+
+
+
+    return 0;
+}
